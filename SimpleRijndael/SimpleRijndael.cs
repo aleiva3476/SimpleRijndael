9a1fb5001b6f232ad8dd68ae219b9351f0c6fd98
Antonio Leiva <antonio.leiva@gmail.com>
2017-06-09 10:38:45 +0200

    Add files via upload
    Inicialización repositorio

---
+++ SimpleRijndael/SimpleRijndael.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Alp3476
+{
+    public static class SimpleRijndael
+    {
+        public static string Encrypt(string inputText, string password)
+        {
+            string encryptedData = string.Empty;
+            using (RijndaelManaged rij = new RijndaelManaged())
+            {
+                byte[] inputBytes = Encoding.Unicode.GetBytes(inputText);
+                byte[] salt = Encoding.ASCII.GetBytes(password.Length.ToString());
+
+                PasswordDeriveBytes secretKey = new PasswordDeriveBytes(password, salt);
+                ICryptoTransform encryptor = rij.CreateEncryptor(secretKey.GetBytes(32), secretKey.GetBytes(16));
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+                    cs.Write(inputBytes, 0, inputBytes.Length);
+                    cs.FlushFinalBlock();
+                    byte[] cipherBytes = ms.ToArray();
+                    encryptedData = Convert.ToBase64String(cipherBytes);
+                }
+            }
+
+            return encryptedData;
+        }
+
+        public static string Decrypt(string inputText, string password)
+        {
+            string decryptedData = string.Empty;
+
+            using (RijndaelManaged rij = new RijndaelManaged())
+            {
+                byte[] encryptedData = Convert.FromBase64String(inputText);
+
+                byte[] salt = Encoding.ASCII.GetBytes(password.Length.ToString());
+                PasswordDeriveBytes secretKey = new PasswordDeriveBytes(password, salt);
+                ICryptoTransform decryptor = rij.CreateDecryptor(secretKey.GetBytes(32), secretKey.GetBytes(16));
+
+                using (MemoryStream ms = new MemoryStream(encryptedData))
+                {
+                    CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+                    byte[] plainText = new byte[encryptedData.Length];
+                    int decryptedCount = cs.Read(plainText, 0, plainText.Length);
+                    decryptedData = Encoding.Unicode.GetString(plainText, 0, decryptedCount);
+                }
+            }
+
+            return decryptedData;
+
+        }
+    }
+}
---
+++ TestRinjdael/TestRinjdael/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Alp3476;
+
+namespace TestRinjdael
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "This is a text to encrypt!";
+            string password = "ThisIsMyPassword";
+
+
+            string encriptado = SimpleRijndael.Encrypt(original, password);
+            string desencriptado = SimpleRijndael.Decrypt(encriptado, password);
+
+            Console.WriteLine("Encriptado:");
+            Console.WriteLine(encriptado);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Desencriptado:");
+            Console.WriteLine(desencriptado);
+
+            Console.ReadKey();
+
+        }
+    }
+}
